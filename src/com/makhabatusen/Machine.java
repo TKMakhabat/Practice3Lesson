b96f91f27c5c04e1646f2c63552c184abdc835ec
UsenM <usen.makhabat@gmail.com>
2021-10-02 14:30:20 +0600

    modified

--- src/com/makhabatusen/Machine.java
+++ src/com/makhabatusen/Machine.java
@@ -1,13 +1,8 @@
 package com.makhabatusen;
 
-public class Machine implements Plant {
+public class Machine {
 
-    void  start() {
+    void start() {
         System.out.println("Starting this machine");
-    }
-
-    @Override
-    public void grow() {
-        System.out.println("Machine grows the plant");
     }
 }
--- src/com/makhabatusen/Main.java
+++ src/com/makhabatusen/Main.java
@@ -10,20 +10,10 @@ public class Main {
 
         CPU cpu = new CPU();
 
-        CPU.Processor processor = cpu. new Processor() {
-            @Override
-            public void setType(String type) {
-                super.setType(type);
-            }
-        };
+        CPU.Processor processor = cpu. new Processor();
 
         processor.setType("Intel Core i9");
-        CPU.RAM ram =  cpu  . new RAM() {
-            @Override
-            public void setSpeed(int speed) {
-                super.setSpeed(speed);
-            }
-        };
+        CPU.RAM ram =  cpu. new RAM();
         ram.setSpeed(2133);
 
         cpu.setProcessorType(processor.getType());
@@ -31,30 +21,22 @@ public class Main {
         System.out.println( cpu.toString());
 
 
-
-
-
-
         System.out.println("______\nTask 2: ");
         // #2 Class Machine implementing Plant interface
 
         Machine machine = new Machine() {
-
             @Override
             void start() {
                 super.start();
                 System.out.println("Starts in main() method");
             }
-
-            @Override
-            public void grow() {
-                super.grow();
-                System.out.println("Grows in main() method");
-            }
         };
 
         machine.start();
-        machine.grow();
+
+        // lambda used to shorten the code
+        Plant plant = () -> System.out.println("Grows in main() method");
+        plant.grow();
 
     }
 }
